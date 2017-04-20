b1418d8977ee8a8c7ab287c486528f01f2f65f8f
Kalcsú Attila <attila.kalcsu@gmail.com>
2017-04-20 11:25:36 +0200

    add+insert+delete

---
+++ doublelinkedlist/CircularDoubleLinkedList.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doublelinkedlist
+{
+    class CircularDoubleLinkedList<T> where T : class
+    {
+        class Header<T> where T : class
+        {
+            public Header<T> Next = null;
+            public Header<T> Prev = null;
+            public T Data = null;
+        }
+
+        Header<T> Head = null;
+
+        public void Add(T Item)
+        {
+            if (Head == null)
+            {
+                Head = new Header<T>()
+                {
+                    Data = Item
+                };
+
+                Head.Next = Head;
+                Head.Prev = Head;
+            }
+            else
+            {
+                Header<T> hdr = new Header<T>
+                {
+                    Data = Item
+                };
+
+                hdr.Prev = Head.Prev;
+                //hdr.Next = Head;
+                hdr.Next = hdr.Prev.Next;
+                hdr.Prev.Next = hdr;
+                hdr.Next.Prev = hdr;
+            }
+        }
+
+        public void Insert(int Index, T Item)
+        {
+            if (Head == null)
+            {
+                Add(Item);
+                return;
+            }
+
+            Header<T> Act = Head;
+            int idx = 0;
+
+            while (idx < Index)
+            {
+                Act = Act.Next;
+                idx++;
+
+                if (Act == Head)
+                {
+                    throw new OverflowException();
+                }
+            }
+
+            Header<T> hdr = new Header<T>
+            {
+                Data = Item
+            };
+
+            hdr.Next = Act;
+            hdr.Prev = Act.Prev;
+
+            // előző álltása
+            //Act.Prev.Next = hdr;
+            hdr.Prev.Next = hdr;
+
+            //Act.Prev = hdr;
+            hdr.Next.Prev = hdr;
+
+            if (Index == 0)
+                Head = hdr;
+        }
+
+        public void Delete(int Index)
+        {
+            if (Head == null)
+            {
+                throw new OverflowException();
+            }
+
+            Header<T> Act = Head;
+            int idx = 0;
+
+            while (idx < Index)
+            {
+                Act = Act.Next;
+                idx++;
+
+                if (Act == Head)
+                {
+                    throw new OverflowException("Te barom, túlindexeltél");
+                }
+            }
+
+            if (Index == 0)
+            {
+                if (Head.Next == Head) // 1 elemű lista
+                {
+                    Head = null;
+                    return;
+                }
+                else
+                    Head = Head.Next;
+            }
+
+            Act.Prev.Next = Act.Next;
+            Act.Next.Prev = Act.Prev;
+        }
+    }
+}
--- doublelinkedlist/Program.cs
+++ doublelinkedlist/Program.cs
@@ -10,6 +10,7 @@ namespace doublelinkedlist
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
